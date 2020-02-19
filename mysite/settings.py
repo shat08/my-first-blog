e9eff973d027ce7c457e7a5bc3dab790ec389e3c
shat <shat080tahs@gmail.com>
2020-02-19 18:39:12 +0900

    template-partyをダウンロードして使ったンゴ

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
+    'pdfmr',
+   
+   
 ]
 
 MIDDLEWARE = [
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

8e03ca941f45beb10efbc3581fdfc4e5d5775d4f
shat <shat080tahs@gmail.com>
2020-02-21 00:01:12 +0900

    changed logo

--- templates/pdfmr/views.py
+++ templates/pdfmr/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-def top(request, pk):
+def top(request):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'pdfmr/top.html', {'post': post}) 

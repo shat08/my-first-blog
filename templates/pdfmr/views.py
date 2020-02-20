78434179372d1f1af42db8d01458a49c4e84c25a
shat <shat080tahs@gmail.com>
2020-02-20 13:35:59 +0900

    サイドバーを変更

--- templates/pdfmr/views.py
+++ templates/pdfmr/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
-def top(request):
+def top(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    return render(request, 'pdfmr/top.html') 
+    return render(request, 'pdfmr/top.html', {'post': post}) 

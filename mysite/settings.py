eb35c2c072acd3e3ed61c627276c695a2da012a4
shat <shat080tahs@gmail.com>
2020-03-12 21:49:16 +0900

    Debug

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '+^&z*=3z1#7*d9vvsj!&4zif!@9rh$faem-k*k=ks6u49x6@i)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','.pythonanywhere.com']
 
